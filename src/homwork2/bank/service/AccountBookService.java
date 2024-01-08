fb2eaa310a9533f9881c45495efd24d12d857207
20231208kh <mimsso@naver.com>
2024-01-08 17:48:01 +0900

    자바 과제 2 : 가계부 프로그램 역할분담

---
+++ src/homwork2/bank/service/AccountBookService.java
@@ -0,0 +1,37 @@
+package homwork2.bank.service;
+
+public interface AccountBookService {
+
+	
+	//회원가입 : 김준수, 박성훈
+	
+	//가계부 수입 관리 : 김태원
+	boolean addDeposit();
+	//수정
+	//날짜+용도
+	//1. 금액
+	//2. 날짜
+	//3. 용도/출처 
+	boolean setDeposit();
+	boolean deleteDeposit();
+	
+	//가계부 지출 관리 : 김태원
+	boolean addWithdraw();
+	boolean setWithdraw();
+	boolean deleteWithdraw();
+	
+	//가계부 조회 관리 : 박석훈
+	boolean searchByDate();
+	//출처별 조회
+	//지출 용도
+	//수입 출처
+	boolean searchByUsage();
+	//1000원이하 지출
+	//1000원이상 10000원이하 지출
+	//10000이상 지출
+	boolean searchByMoney();
+	
+	//전체 조회
+	boolean searchAll();
+	
+}
---
+++ src/homwork2/bank/service/AccountBookServiceImp.java
@@ -0,0 +1,75 @@
+package homwork2.bank.service;
+
+import java.util.List;
+
+import homwork2.bank.Bank;
+
+public class AccountBookServiceImp implements AccountBookService {
+
+	private List<Bank> bankList;
+	
+	
+	
+	@Override
+	public boolean addDeposit() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean setDeposit() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean deleteDeposit() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean addWithdraw() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean setWithdraw() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean deleteWithdraw() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean searchByDate() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean searchByUsage() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean searchByMoney() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean searchAll() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	
+	
+}
